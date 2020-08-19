c79475a82e2b2c5fb2381069aa490c5cf9a9cc65
Sonakshi Rastogi <sonakshirastogi75@gmail.com>
2020-08-18 19:37:59 -0700

    Added project

---
+++ ModernERAServices/Database/Entities/DomainEntities/ModernEraContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ModernERAServices.Database
+{
+    public partial class ModernEraContext : DbContext
+    {
+        public ModernEraContext()
+        {
+        }
+
+        public ModernEraContext(DbContextOptions<ModernEraContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserDetails> UserDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=SONAKSHI\\SQLEXPRESS;Database=ModernEra;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDetails>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.Property(e => e.UserId).ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ContactNo)
+                    .HasColumnName("Contact_No")
+                    .HasColumnType("numeric(18, 0)");
+
+                entity.Property(e => e.EmailId)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.State)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserFirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserLastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ModernERAServices/Database/Entities/DomainEntities/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ModernERAServices.Database
+{
+    public partial class UserDetails
+    {
+        public int UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string EmailId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal? ContactNo { get; set; }
+    }
+}
